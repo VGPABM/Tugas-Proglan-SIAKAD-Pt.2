3b8b7b7ad0a1d64198185cb1f660cb02b116c150
VGPABM <94452395+VGPABM@users.noreply.github.com>
2022-06-11 02:09:55 +0700

    Delete src directory

--- src/matkul.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <vector>
-#include "include/matkul.hpp"
-
-matkul::matkul(std::string namamatkul, std::string dosenpengajar,std::string dosenpengurus,int jumlah_sks, int jumlahkelas, int jumlahsiswaterdaftar)
-            :namamatkul(namamatkul),dosenpengajar(dosenpengajar),deptpengurus(deptpengurus),jumlah_sks(jumlah_sks),jumlahkelas(jumlahkelas),jumlahsiswaterdaftar(jumlahsiswaterdaftar)
-{
-}
-
